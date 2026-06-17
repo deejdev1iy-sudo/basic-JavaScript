51337f152a5bfb8a006e499bf1dd8decda863375
joydannah <deejdev1iy@gmail.com>
2026-06-17 15:47:08 +0300

    Intro to primitive data types

---
+++ JS/intro/primitiveDataType.js
@@ -0,0 +1,26 @@
+// number&floating-num&bigNumber
+let number = 2000;
+let floatingNumber = 0.003;
+let bigNumber = 100293949495896064003957685;
+
+console.log(number);
+console.log(floatingNumber);
+console.log(bigNumber);
+
+// STRINGS
+let str0 = "Double\n quotes are here"
+console.log(str0);
+let str1 = 'Saw some doves fighting'
+console.log(str1);
+let str2 = `Some backticks`
+console.log(str2);
+let str3 = "39"
+console.log("About to hit:", str3);
+let str4 = 'You\'re not everything, "39 said so"'
+console.log(str4);
+
+// null$undefined
+let numberNull = null;
+console.log(numberNull);
+let undefinedPerson;
+console.log(undefinedPerson)
