7389df72b99ec477e11b1c9986c956a52078d1a4
joydannah <deejdev1iy@gmail.com>
2026-06-19 10:13:40 +0300

    Comaprison Operators

---
+++ JS/intro/comparisonOperators.js
@@ -0,0 +1,27 @@
+/*
+Comparison Operators evaluate to only true or false.
+Equality ==
+Strict Equality ===
+Inequality !=
+Strict Inequality !==
+*/
+//Equality ==
+//Strict Equality ===
+let num2 = "77"
+let num3 = 100
+let strictEquality = num2 === num3
+console.log(`num2 = ${num2} === num3 = ${num3} => Ans ${strictEquality}, its type is ${typeof strictEqual}`)
+
+let nonStrictEquality = num2 == num3
+console.log(`num3 = ${num3} == num3 = ${num3} => Ans ${nonStrictEquality}, its type is ${typeof nonStrictEquality}`)
+
+//Inequality !=
+//Strict Inequality !==
+let val1 = 30
+let val2 = "30"
+
+let strictInequality = val1 !==val2
+console.log(`val1 = ${val1} !== val2 = ${val2} => Ans ${strictInequality}, its type is ${typeof strictInequality}`)
+
+let Inequality = val1 != val2
+console.log(`val1 = ${val1} != val2 = ${val2} => Ans ${Inequality}, its type is ${typeof Inequality}`)
