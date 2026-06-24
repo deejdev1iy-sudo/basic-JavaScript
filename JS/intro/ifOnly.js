665ff4b8c8feb025dcb7b2031019347e1cc62278
joydannah <deejdev1iy@gmail.com>
2026-06-24 10:52:05 +0300

    IfOnly statements

---
+++ JS/intro/ifOnly.js
@@ -0,0 +1,27 @@
+/*
+ its speacial->you  use it own its own
+if is indepedent
+one statement doesnt depend on another
+*/
+let age = prompt("Enter your age")
+alert(`You entered ${age}`)
+
+if(age>10){
+  alert("You are a baby")
+
+};
+if(age>13){
+    alert("You are a teen")
+};
+if(age>18){
+    alert("You can legally drink&smoke")
+}
+if(age>27){
+    alert("Appropriate age for marriage")
+}
+if(age>35){
+    alert("Best age for early retirement")
+}
+if(age>50){
+    alert("You've aged fracefully")
+}
