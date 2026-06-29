f9b394b757c2a2e1780875ffc05adbad4330f18c
joydannah <deejdev1iy@gmail.com>
2026-06-29 10:28:12 +0300

    Test try

--- JS/intro/Functions/introFunction.js
+++ JS/intro/Functions/introFunction.js
@@ -8,3 +8,19 @@ function timeAlert(){
     console.log(`time stamp: ${date.toISOString()}`);
 }
 timeAlert();
+function areaOfTriangle(base, height){
+    console.log(`base is ${base} its type ${typeof base}`)
+    console.log(`height is ${height} its type ${typeof height}`)
+    const area = 0.5 * base * height;
+    console.log(`for triangle with base ${base} and height ${height} area is ${area}`)
+};
+//scenario 1
+areaOfTriangle();
+// scenario 2
+areaOfTriangle(20);
+//scenario 3
+areaOfTriangle(30,45);
+//scenario 4
+let base1 = 100;
+let height1 = 50;
+areaOfTriangle(base1, height1);
