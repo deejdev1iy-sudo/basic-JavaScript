1c2b0121d18ca1b36387e54cef9457bcc83995ba
joydannah <deejdev1iy@gmail.com>
2026-07-09 10:32:27 +0300

    Intro to Objects

---
+++ JS/intro/masterjs/introObjects.js
@@ -0,0 +1,43 @@
+/*
+ts a datatype that houses othe datatypes
+key: value pair
+key->number/string<should be unique>
+value-> any  other datatype
+*/
+
+// create an object called car
+const car = {
+   name: 'Model Y',
+   model: 'Long Range AWD',
+   yearManufactured: 2026,
+   topSpeedKmh: 217,
+
+   engine: {
+    type: 'Electric',
+    cylinders: 0,
+    propulsion: 'Dual Electric Motors (front & rear)',
+    batteryType: 'Lithium-ion',
+    fuelType: 'Electricity',
+   },
+
+   manufacturer: {
+    name2: 'Tesla, Inc',
+    country: 'United States'
+   },
+
+   funFacts:`
+    It features a 'Bioweapon Defense Mode' with a HEPA air filtration system.
+    It was the best-selling vehicle globally, beating all gas-powered cars.
+    The paranomic glass roof blocks over 99% of ultraviolet light.
+   `,
+
+   printInfo: function(){
+    console.log(`Car: ${this.manufacturer.name2} ${this.name} (${this.model})`);
+    console.log(`Year: ${this.yearManufactured}`);
+    console.log(`Top Speed: ${this.topSpeedKmh} km/h`);
+    console.log(`Engine Type: ${this.engine.type} (${this.engine.propulsion})`);
+    console.log(`Origin: Made in the ${this.manufacturer.country}`);
+    console.log(`Fun Facts: These are ${this.funFacts}`);
+   }
+}
+car.printInfo();
