3e673ddb7fcbee5ebd15eacb46dfbbaa52706974
joydannah <deejdev1iy@gmail.com>
2026-08-26 10:55:25 +0300

    Add condition and loop examples in new Python script

---
+++ JS/Python/condition_and_loop.py
@@ -0,0 +1,14 @@
+age = 20
+
+if age < 18:
+    print("You can drink")
+
+for i in range(0, 10, 2):
+    print("I is", i)
+
+arr = [30, 'new', True, 45, 56]
+for i in range(0, len(arr)):
+    single_item = arr[i]
+    print(single_item)    
+
+    
---
+++ JS/Python/function.py
@@ -0,0 +1,6 @@
+def area_circle(radius):
+    ans = 3.14 * pow(radius,2)
+    print(f"for circle of radius {radius} area is {ans}")
+    return
+
+area_circle(40)
