b1b53e70af2deeff811595adfade5dc4a004aa26
joydannah <deejdev1iy@gmail.com>
2026-09-01 10:32:05 +0300

    Add House class with configuration and print functionality in Python

---
+++ JS/Python/object_oriented_progm/intro.py
@@ -0,0 +1,25 @@
+class House:
+    architect = 'Sonnia'
+    bedrooms = 4
+    bathrooms = 4
+    floors = 3
+    area = 150
+    owner = ''
+    location = ''
+
+    def config(self, owner, location):
+        self.owner=owner
+        self.location=location
+
+    def print_self(self):
+        print(self)
+        print(self.__dict__)    
+
+Dees_House =  House()
+Dees_House.config(owner='Mooney', location='Tigoni')
+print(f"Dees House owner, {Dees_House.owner}")
+print(f"Dees House location, {Dees_House.location}")
+print(f"Dees House bedrooms, {Dees_House.bedrooms}")
+print(f"Dees House bathrooms, {Dees_House.bathrooms}")
+Dees_House.print_self()
+
