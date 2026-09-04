56f4aac8c4552574cd9755742615737386d3d482
joydannah <deejdev1iy@gmail.com>
2026-09-04 10:27:28 +0300

    Add static and class methods for interest calculation and client management in BankAccount class

--- JS/Python/object_oriented_progm/bank.py
+++ JS/Python/object_oriented_progm/bank.py
@@ -33,6 +33,19 @@ class BankAccount:
         print(f"Balance {self.balance}")
         print(f"Account no {self.account_no}")
 
+    @staticmethod
+    def calculate_interest(amount, year):
+        rate = 10
+        interest_per_year = amount*(year/100)
+        interest_total = interest_per_year * year
+        total = amount + interest_total  
+        print(f"If you take a loan. of ${amount}, interest rate per year {interest_per_year}")
+        print(f"Total interest {interest_total}, total to pay is {total} after {year}")
+
+    @classmethod
+    def add_client(cls):
+        cls.client = cls.client + 1
+
 jerry = BankAccount(owner = "Jerry",balance = 200, account_no = '1298965470')
 
 print("Jerry's balance", jerry.balance )   
